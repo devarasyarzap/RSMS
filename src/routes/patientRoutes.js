ee95fbc00d4d76d036377fd85c27feb682aa5ee4
nusasofyan <nusa.sofyan@mhs.itenas.ac.id>
2026-01-18 17:43:56 +0700

    percobaan hosting

--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
 
-
 router.get('/data', patientController.getAllPatients);
 
 router.post('/create', patientController.createPatientOffline);
