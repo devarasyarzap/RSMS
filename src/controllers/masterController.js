e7f7ff32916d26ff4839ee2c4c476aa33727b920
Devarasya Rizky Amelia Putri <devarasya.rizky@mhs.itenas.ac.id>
2026-01-19 08:21:16 +0700

    menambahkan medical record

--- src/controllers/masterController.js
+++ src/controllers/masterController.js
@@ -4,6 +4,7 @@ const {
   User,
   WardClass,
   Bed,
+  MedicalRecord,
 } = require("../models/associations");
 
 exports.getAllUsers = async (req, res) => {
@@ -12,6 +13,15 @@ exports.getAllUsers = async (req, res) => {
       order: [["createdAt", "DESC"]],
     });
     res.status(200).json({ status: "success", data: users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getAllMedical = async (req, res) => {
+  try {
+    const medical = await MedicalRecord.findAll();
+    res.status(200).json({ status: "success", data: medical });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/routes/masterRoutes.js
+++ src/routes/masterRoutes.js
@@ -1,36 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const masterController = require('../controllers/masterController');
+const masterController = require("../controllers/masterController");
 
-router.get('/users', masterController.getAllUsers);
+router.get("/users", masterController.getAllUsers);
+
+router.get("/medical-records", masterController.getAllMedical);
 
 // POST /api/master/polys (Tambah Poli)
-router.post('/polys', masterController.createPolyclinic);
+router.post("/polys", masterController.createPolyclinic);
 
 // GET /api/master/polys  (Ambil Semua)
-router.get('/polys', masterController.getAllPolyclinics);
+router.get("/polys", masterController.getAllPolyclinics);
 
 // GET /api/master/polys:id (Ambil Satu + Dokter)
-router.get('/polys:id', masterController.getPolyclinicDetail);
+router.get("/polys:id", masterController.getPolyclinicDetail);
 
 // POST /api/master/doctors (Tambah Dokter)
-router.post('/doctors', masterController.createDoctorProfile);
+router.post("/doctors", masterController.createDoctorProfile);
 
 // GET /api/master/doctors (Lihat semua dokter)
-router.get('/doctors', masterController.getAllDoctors);
+router.get("/doctors", masterController.getAllDoctors);
 
 // GET /api/master/doctors:id (Lihat detail 1 dokter)
-router.get('/doctors:id', masterController.getDoctorById);
+router.get("/doctors:id", masterController.getDoctorById);
 
-router.get('/ward-classes', masterController.getWards);
+router.get("/ward-classes", masterController.getWards);
 
 // POST /api/master/ward-classes (Tambah Kelas)
-router.post('/ward-classes', masterController.createWardClass);
+router.post("/ward-classes", masterController.createWardClass);
 
 // POST /api/master/beds (Tambah Kasur)
-router.post('/beds', masterController.createBed);
+router.post("/beds", masterController.createBed);
 
 // GET /api/master/beds (Lihat Daftar Kasur)
-router.get('/beds', masterController.getAllBeds);
+router.get("/beds", masterController.getAllBeds);
 
 module.exports = router;
